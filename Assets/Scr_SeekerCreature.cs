3d3160092f895b2cf3265ff86589bdc814717f7e
DunniDee <65572018+DunniDee@users.noreply.github.com>
2022-07-10 00:18:40 +1200

    Creatre moving

--- Assets/Scr_SeekerCreature.cs
+++ Assets/Scr_SeekerCreature.cs
@@ -1,10 +1,14 @@
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Scr_SeekerCreature : MonoBehaviour
 {
+    [SerializeField] Transform MechPos;
+    [SerializeField] Rigidbody RB;
 
+    [SerializeField] Transform LookTowards;
 
     // Start  is called before the first frame update
     void Start()
@@ -15,6 +19,9 @@ public class Scr_SeekerCreature : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
+        LookTowards.LookAt(MechPos);
+        transform.rotation = Quaternion.Lerp(transform.rotation,LookTowards.rotation, Time.deltaTime * 5);
+        RB.AddForce(transform.forward * 5, ForceMode.Acceleration);
+        RB.AddForce(-RB.velocity * 0.8f, ForceMode.Acceleration);
     }
 }
