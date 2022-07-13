57ce236bd082533e1be38453314e9ad1c699e730
Noobcoodoocoo <noobcoodoocoo@gmail.com>
2022-07-13 21:14:39 +1200

    adding radar to player prefab

---
+++ Assets/Headlightculling.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Headlightculling : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //public GameObject limelight;
+
+    void Start()
+    {
+       /* function OnPreCull()
+        {
+            if (limelight != null)
+            {
+                limelight.enabled = false;
+
+            }
+                
+        }
+
+        function OnPreRender()
+        {
+            if (limelight != null)
+            {
+                limelight.enabled = false;
+
+            }
+                
+        }
+        function OnPostRender()
+        {
+            if (limelight != null)
+            {
+                limelight.enabled = true;
+
+            }
+                
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    
+
+}
